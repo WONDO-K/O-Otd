798de633207fe4ebbed751812a959bf053b07740
huggycareer <huggycareer@gmail.com>
2024-09-20 16:39:11 +0900

    [Comment] KafkaTemplate 주석 해제

--- be/user/ootd/src/main/java/com/threeheads/ootd/kafka/producer/config/KafkaProducerConfigCluster.java
+++ be/user/ootd/src/main/java/com/threeheads/ootd/kafka/producer/config/KafkaProducerConfigCluster.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -30,10 +31,10 @@ public class KafkaProducerConfigCluster {
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
-//    @Bean
-//    public KafkaTemplate<String, KafkaEntity> kafkaTemplate(){
-//        return new KafkaTemplate<>(producerFactory());
-//    }
+    @Bean
+    public KafkaTemplate<String, KafkaEntity> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
 
 
 }
