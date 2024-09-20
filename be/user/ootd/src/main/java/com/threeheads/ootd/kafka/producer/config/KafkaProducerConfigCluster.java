12f3ca927e0d55471ecd264e9fe1616db7f3a5b9
huggycareer <huggycareer@gmail.com>
2024-09-20 16:00:24 +0900

    [Refactor] KafkaConsumerConfigCluster package 구조 변경

--- be/user/ootd/src/main/java/com/threeheads/ootd/kafka/producer/config/KafkaProducerConfigCluster.java
+++ be/user/ootd/src/main/java/com/threeheads/ootd/kafka/producer/config/KafkaProducerConfigCluster.java
@@ -1,4 +1,4 @@
-package com.threeheads.ootd.kafka.config;
+package com.threeheads.ootd.kafka.producer.config;
 
 import com.threeheads.ootd.kafka.entity.KafkaEntity;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
