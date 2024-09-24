a2851c8a0710cfd058e57f6ff386206bb0b3a2e4
rt21hyuk <rt21hyuk@gmail.com>
2024-09-24 17:07:24 +0900

    [Init] 파일 구조 및 파라미터 설정

---
+++ sw/bigdata/settings.py
@@ -0,0 +1,13 @@
+DATA_FILE = 'umap_clustering_data.npz'
+CLUSTER_CENTERS_FILE = 'cluster_centers.npy'
+CLUSTER_DATA_DIR = 'cluster_data/'
+UMAP_MODEL_FILE = 'umap_model.pkl'
+
+IMAGE_DIR = 'images'
+IMAGE_SECTION_FILE = 'section_clustering.png'
+IMAGE_FULL_FILE = 'full_clustering.png'
+IMAGE_RESULT_FILE = 'result_clustering.png'
+
+n_samples = 30000
+n_features = 6
+n_clusters = 6
