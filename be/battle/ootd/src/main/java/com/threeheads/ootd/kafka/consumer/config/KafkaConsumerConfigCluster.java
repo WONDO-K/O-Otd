8504003156089c66d3e2bc8022a300caad2d8e5b
huggycareer <huggycareer@gmail.com>
2024-09-25 17:23:45 +0900

    [Fix] User Consumer group으로 통합/수정

--- be/battle/ootd/src/main/java/com/threeheads/ootd/kafka/consumer/config/KafkaConsumerConfigCluster.java
+++ be/battle/ootd/src/main/java/com/threeheads/ootd/kafka/consumer/config/KafkaConsumerConfigCluster.java
@@ -16,66 +16,37 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConsumerConfigCluster {
-    // kafka 서버 주소 application.properties 파일에서 들고 옴
+    // Kafka 서버 주소를 애플리케이션 설정 파일에서 가져와서 변수에 저장
+
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
 
-    // TODO: 소비자 그훕 ID 설정
-    // Kafka 소비자 그룹 ID를 application.properties 파일에서 들고 옴
-    @Value("${spring.kafka.consumer.group-id-battle-user}")
-    private String battleuser;
+    // Kafka 소비자 그룹 ID를 애플리케이션 설정 파일에서 가져와서 변수에 저장
 
-    @Value("${spring.kafka.consumer.group-id-battle-gallery}")
-    private String battlegallery;
+    // user가 battle에서 메시지를 받아올 때 사용하는 그룹
+    @Value("${spring.kafka.consumer.group-battle}")
+    private String battleGroup;
 
     // KafkaEntity 타입의 메시지를 소비하기 위한 ConsumerFactory를 생성하는 Bean
-    // ConsumerFactory for Battle User
     @Bean
-    public ConsumerFactory<String, KafkaEntity> pushEntityBattleUserFactory() {
+    public ConsumerFactory<String, KafkaEntity> pushEntityConsumerFactory() {
         // KafkaEntity를 역직렬화하는 데 사용할 JsonDeserializer 객체를 생성
         JsonDeserializer<KafkaEntity> deserializer = gcmPushEntityJsonDeserializer();
         // 생성한 JsonDeserializer와 설정값들을 사용해 DefaultKafkaConsumerFactory 객체를 반환
         return new DefaultKafkaConsumerFactory<>(
-                battleUserConsumerFactoryConfig(deserializer),
+                userConsumerFactoryConfig(deserializer),
                 new StringDeserializer(), // 키를 위한 디시리얼라이저 (String 타입)
                 deserializer);            // 값을 위한 디시리얼라이저 (KafkaEntity 타입)
     }
-
-    // ConsumerFactory for Battle Gallery
-    @Bean
-    public ConsumerFactory<String, KafkaEntity> pushEntityBattleGalleryFactory() {
-        // KafkaEntity를 역직렬화하는 데 사용할 JsonDeserializer 객체를 생성
-        JsonDeserializer<KafkaEntity> deserializer = gcmPushEntityJsonDeserializer();
-        // 생성한 JsonDeserializer와 설정값들을 사용해 DefaultKafkaConsumerFactory 객체를 반환
-        return new DefaultKafkaConsumerFactory<>(
-                battleGalleryConsumerFactoryConfig(deserializer),
-                new StringDeserializer(), // 키를 위한 디시리얼라이저 (String 타입)
-                deserializer);            // 값을 위한 디시리얼라이저 (KafkaEntity 타입)
-    }
-
 
     // ConsumerFactory를 구성하는 설정값들을 Map으로 반환하는 메서드
-    // user에서 batlle로 보낼때 사용하는 consumergroup 설정
-    private Map<String, Object> battleUserConsumerFactoryConfig(JsonDeserializer<KafkaEntity> deserializer) {
+    // User->battle
+    private Map<String, Object> userConsumerFactoryConfig(JsonDeserializer<KafkaEntity> deserializer) {
         Map<String, Object> props = new HashMap<>();
         // Kafka 서버 주소 설정
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         // Kafka 소비자 그룹 ID 설정
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, battleuser);
-        // 메시지 키를 직렬화/역직렬화하는 클래스 설정 (String 타입)
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        // 메시지 값을 직렬화/역직렬화하는 클래스 설정 (KafkaEntity 타입)
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
-        return props;
-    }
-
-    // gallelry에서 batlle로 보낼때 사용하는 consumergroup 설정
-    private Map<String, Object> battleGalleryConsumerFactoryConfig(JsonDeserializer<KafkaEntity> deserializer) {
-        Map<String, Object> props = new HashMap<>();
-        // Kafka 서버 주소 설정
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        // Kafka 소비자 그룹 ID 설정
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, battleuser);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, battleGroup);
         // 메시지 키를 직렬화/역직렬화하는 클래스 설정 (String 타입)
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         // 메시지 값을 직렬화/역직렬화하는 클래스 설정 (KafkaEntity 타입)
@@ -96,27 +67,15 @@ public class KafkaConsumerConfigCluster {
         return deserializer;
     }
 
-    // User에서 Battle로 들어오는 Kafka 메시지를 처리하는 Listener 컨테이너 팩토리를 생성하는 Bean
+    // Kafka 메시지를 처리하는 Listener 컨테이너 팩토리를 생성하는 Bean
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, KafkaEntity>
-    battleUserKafkaListenerContainerFactory() {
+    kafkaListenerContainerFactory() {
         // 새로운 ConcurrentKafkaListenerContainerFactory 객체 생성
         ConcurrentKafkaListenerContainerFactory<String, KafkaEntity> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         // 위에서 생성한 ConsumerFactory를 사용하도록 설정
-        factory.setConsumerFactory(pushEntityBattleUserFactory());
-        return factory;
-    }
-
-    // gallery에서 Battle로 들오어는 Kafka 메시지를 처리하는 Listener 컨테이너 팩토리를 생성하는 Bean
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, KafkaEntity>
-    battleGalleryKafkaListenerContainerFactory() {
-        // 새로운 ConcurrentKafkaListenerContainerFactory 객체 생성
-        ConcurrentKafkaListenerContainerFactory<String, KafkaEntity> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        // 위에서 생성한 ConsumerFactory를 사용하도록 설정
-        factory.setConsumerFactory(pushEntityBattleGalleryFactory());
+        factory.setConsumerFactory(pushEntityConsumerFactory());
         return factory;
     }
 }
