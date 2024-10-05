1c6ff0b933ff832a4819c59411bb9680637cb3d5
huggycareer <huggycareer@gmail.com>
2024-10-05 20:16:37 +0900

    [Feat] 찜(MyLike) entity생성

---
+++ be/gallery/ootd/src/main/java/com/threeheads/gallery/model/entity/MyLike.java
@@ -0,0 +1,26 @@
+package com.threeheads.gallery.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="MyLike")
+public class MyLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long clothesId;
+
+    @Column(name="user_id",nullable = false)
+    private int userId;
+
+    @Column(name = "like_date",nullable = false)
+    private LocalDateTime likeDateTime;
+
+}
