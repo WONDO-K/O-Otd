1ea08f3284dfac6f67c7cf0334d2b80d4029fff9
huggycareer <huggycareer@gmail.com>
2024-10-08 11:22:44 +0900

    [Feat] Add Service

--- be/gallery/ootd/src/main/java/com/threeheads/gallery/model/service/GalleryServiceImpl.java
+++ be/gallery/ootd/src/main/java/com/threeheads/gallery/model/service/GalleryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.threeheads.gallery.model.service;
 
+import com.threeheads.gallery.model.dto.GalleryListResponseDto;
 import com.threeheads.gallery.model.entity.Gallery;
 import com.threeheads.gallery.model.entity.MyLike;
 import com.threeheads.gallery.model.dto.AddCollectionDto;
@@ -9,6 +10,7 @@ import com.threeheads.gallery.model.repository.GalleryRepository;
 import com.threeheads.gallery.model.repository.LikeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -72,14 +74,60 @@ public class GalleryServiceImpl implements GalleryService {
     @Override
     public GalleryDetailDto getGalleryDetail(int galleryId) {
         GalleryDetailDto galleryDetailDto = new GalleryDetailDto();
-        Gallery gallery = galleryRepository.findGalleryDetailByGalleyId(galleryId);
+        Gallery gallery = galleryRepository.findGalleryDetailByGalleryId(galleryId);
+        galleryDetailDto.setCategory(gallery.getType());
         galleryDetailDto.setImageUrl(gallery.getPhotoUrl());
         galleryDetailDto.setImageId(gallery.getGalleryId());
         galleryDetailDto.setLikesCount(gallery.getLikesCount());
         galleryDetailDto.setUploadedAt(gallery.getUploadedAt());
-        // TODO: 분류에서 가져오기
-        // galleryDetailDto.getCategory("");
-
+        log.info("galleryDetailDto: {}",  galleryDetailDto); // 로그 출력
         return galleryDetailDto;
+    }
+
+    @Override
+    public List<GalleryListResponseDto> getGalleryList(String type) {
+        List<Gallery> gallerylist;
+        List<GalleryListResponseDto> result;
+        if(!type.equals(""))
+            gallerylist = galleryRepository.findGalleryListByType(type);
+        else
+            gallerylist = galleryRepository.findGalleryRandomList();
+
+
+        result=gallerylist.stream().map(clothes -> new GalleryListResponseDto(
+            clothes.getGalleryId(),
+            clothes.getPhotoUrl(),
+            clothes.getType(),
+            clothes.getUploadedAt())).toList();
+
+        for(GalleryListResponseDto data:result)
+            log.info("galleryDetailDto: {}",  data);
+        return result;
+    }
+
+    @Override
+    public boolean delMyCollection(long clothesId) {
+        try{
+            likeRepository.deleteById(clothesId);
+            return true;
+        }
+        catch (EmptyResultDataAccessException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public List<Gallery> searchLook(String type) {
+        List<Gallery> result;
+        result=galleryRepository.findGalleryListByType(type);
+        return result;
+    }
+
+    @Override
+    public List<Gallery> getWeekPick() {
+        List<Gallery> result;
+        result = galleryRepository.findWeekPick();
+        return result;
     }
 }
