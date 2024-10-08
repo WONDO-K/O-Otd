630445d357fae1eeda874e7d283404dad1200641
huggycareer <huggycareer@gmail.com>
2024-10-08 15:22:07 +0900

    [Fix] url 수정

--- be/gallery/ootd/src/main/java/com/threeheads/gallery/controller/GalleryRestController.java
+++ be/gallery/ootd/src/main/java/com/threeheads/gallery/controller/GalleryRestController.java
@@ -8,7 +8,6 @@ import com.threeheads.gallery.model.dto.CollectionDto;
 import com.threeheads.gallery.model.dto.GalleryDetailDto;
 import com.threeheads.gallery.model.service.GalleryServiceImpl;
 import lombok.AllArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +19,7 @@ import java.util.List;
 public class GalleryRestController {
     private GalleryServiceImpl service;
 
+    // 마이 컬럭션 추가
     @PostMapping("/my-collection")
     public ResponseEntity<?> addCollection(@RequestBody AddCollectionDto dto){
         MyLike result =  service.addCollection(dto);
@@ -30,6 +30,7 @@ public class GalleryRestController {
         }
     }
 
+    // 마이 컬랙션 리스트조회
     @GetMapping("/my-collection/{userId}")
     public ResponseEntity<?> getCollectionList(@PathVariable("userId") Long userId){
         List<CollectionDto> list = service.getCollectionList(userId);
@@ -39,19 +40,21 @@ public class GalleryRestController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("getCollectionList 에러");
     }
 
+    // 갤러리 상세 조회
     @GetMapping("/{galleryId}")
     public ResponseEntity<?> getGalleryDetail(@PathVariable("galleryId") int galleryId){
         GalleryDetailDto galleryDetailDto = service.getGalleryDetail(galleryId);
         return ResponseEntity.ok(galleryDetailDto);
     }
-
-    @GetMapping("/gallery/list")
+    // 갤러리 지연로딩 리스크 조회
+    @GetMapping("/list")
     public ResponseEntity<?> getGalleryList(@RequestParam String type){
         List<GalleryListResponseDto> result = service.getGalleryList(type);
         return ResponseEntity.ok(result);
     }
 
-    @DeleteMapping("/gallery/my-collection")
+    // 마이 컬럭션 제거
+    @DeleteMapping("/my-collection")
     public ResponseEntity<?> delMyCollection(@RequestParam long clothesId){
         boolean result = service.delMyCollection(clothesId);
         if(result)
@@ -60,13 +63,15 @@ public class GalleryRestController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("delMyCollection 에러");
     }
 
+    // 룩검색
     @GetMapping("/gallery")
     public ResponseEntity<?> searchLook(@RequestParam String type){
         List<Gallery> result = service.searchLook(type);
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping("/gallery/week-pick")
+    // 금주의 픽
+    @GetMapping("/week-pick")
     public ResponseEntity<?> getWeekPick(){
         List<Gallery> result = service.getWeekPick();
         return ResponseEntity.ok(result);
