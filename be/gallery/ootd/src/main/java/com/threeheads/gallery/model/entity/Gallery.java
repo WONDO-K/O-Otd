6cd147c4ff98d015017cf594eddda19cfbee3ed8
huggycareer <huggycareer@gmail.com>
2024-10-08 11:15:39 +0900

    [Fix] gallery Entity column 추가

--- be/gallery/ootd/src/main/java/com/threeheads/gallery/model/entity/Gallery.java
+++ be/gallery/ootd/src/main/java/com/threeheads/gallery/model/entity/Gallery.java
@@ -17,12 +17,21 @@ public class Gallery {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int galleryId;
 
+    @Column(name="photo_name",nullable = false)
+    private String photoName;
+
     @Column(name="photo_url",nullable = false)
     private String photoUrl;
 
     @Column(name="likes_count",nullable = false)
-    String likesCount;
+    int likesCount;
 
     @Column(name="uploaded_at",nullable = false)
     LocalDateTime uploadedAt;
+
+    @Column(name="type",nullable = false)
+    String type;
+
+    @Column(name="is_delete",nullable=false)
+    boolean isDelete = false;
 }
