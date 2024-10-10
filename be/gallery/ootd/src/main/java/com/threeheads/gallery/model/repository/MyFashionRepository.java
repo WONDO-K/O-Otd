bc58286bc58c1eab45c9a0ece82ee7d3bbefc69a
huggycareer <huggycareer@gmail.com>
2024-10-10 10:32:10 +0900

    [Feat] findByUserId repository 작성

--- be/gallery/ootd/src/main/java/com/threeheads/gallery/model/repository/MyFashionRepository.java
+++ be/gallery/ootd/src/main/java/com/threeheads/gallery/model/repository/MyFashionRepository.java
@@ -4,6 +4,9 @@ import com.threeheads.gallery.model.entity.MyFashion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MyFashionRepository extends JpaRepository<MyFashion,Integer> {
+    List<MyFashion> findByUserId(int user_id);
 }
